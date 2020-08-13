2b59b5c249648e68185c0e5e3dc6e08497193aba
Jisun97 <jisunok0907@gmail.com>
2020-08-13 12:20:59 +0900

    0813

--- app/src/main/java/com/david0926/edcansummer/MemoFragment.java
+++ app/src/main/java/com/david0926/edcansummer/MemoFragment.java
@@ -1,17 +1,30 @@
 package com.david0926.edcansummer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableArrayList;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.david0926.edcansummer.databinding.FragmentMemoBinding;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Locale;
 
 public class MemoFragment extends Fragment {
 
@@ -19,9 +32,11 @@ public class MemoFragment extends Fragment {
         return new MemoFragment();
     }
 
+    private ObservableArrayList<MemoModel> items = new ObservableArrayList<>();
+
     private Context mContext;
     private FragmentMemoBinding binding;
-
+    private FirebaseFirestore firebaseFirestore= FirebaseFirestore.getInstance();
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
@@ -33,7 +48,55 @@ public class MemoFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_memo, container, false);
 
+        binding.recyclerMemo.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
+
+        MemoAdapter adapter = new MemoAdapter();
+        binding.recyclerMemo.setAdapter(adapter);
+
+        adapter.setOnItemClickListener((view,item)->{
+            Intent intent = new Intent(mContext, NewMemoActivity.class);
+            intent.putExtra("memo_text", item.getText());
+            startActivity(intent);
+        });
+        adapter.setOnItemLongClickListener((view,item)->{
+
+            return true;
+        });
+
+        binding.setItems(items);
+
+        getMemos();
+
+        binding.fabMemo.setOnClickListener(view->startActivity(new Intent(mContext, NewMemoActivity.class)));
 
         return binding.getRoot();
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        getMemos();
+    }
+
+    private void getMemos(){
+        items.clear();
+        firebaseFirestore
+                .collection("memo")
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    for (DocumentSnapshot d : queryDocumentSnapshots){
+                        items.add(d.toObject(MemoModel.class));
+                    }
+
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd hh:mm aa", Locale.ENGLISH);
+                    Collections.sort(items, (memoModel, t1) -> {
+                        try {
+                            return format.parse(t1.getTime()).compareTo(format.parse(memoModel.getTime()));
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                            return 0;
+                        }
+                    });
+                })
+                .addOnFailureListener(e-> Toast.makeText(mContext,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show());
     }
 }
--- app/src/main/java/com/david0926/edcansummer/MoreFragment.java
+++ app/src/main/java/com/david0926/edcansummer/MoreFragment.java
@@ -1,10 +1,13 @@
 package com.david0926.edcansummer;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +16,8 @@ import androidx.fragment.app.Fragment;
 
 import com.david0926.edcansummer.databinding.FragmentMemoBinding;
 import com.david0926.edcansummer.databinding.FragmentMoreBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MoreFragment extends Fragment {
 
@@ -20,6 +25,8 @@ public class MoreFragment extends Fragment {
         return new MoreFragment();
     }
 
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore firebaseFirestore= FirebaseFirestore.getInstance();
     private Context mContext;
     private FragmentMoreBinding binding;
 
@@ -29,12 +36,44 @@ public class MoreFragment extends Fragment {
         mContext = context;
     }
 
-    @Nullable
-    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_more, container, false);
+        binding.setUser(UserCache.getUser(mContext));//학교 ㅠㅠ
+        binding.btnMoreLogout.setOnClickListener(view -> logout());
 
+        binding.btnMorePw.setOnClickListener(view ->{
+            firebaseAuth.sendPasswordResetEmail(UserCache.getUser(mContext).getEmail());
+            Toast.makeText(mContext, "재설정 이메일이 전송되었습니다!", Toast.LENGTH_SHORT).show();
+            logout();
+        });
+
+        binding.btnMoreDelete.setOnClickListener(view ->{
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setTitle("회원 탈퇴");
+            builder.setMessage("정말로 탈퇴하시겠습니까");
+            builder.setPositiveButton("탈퇴하기", (dialogInter, i) ->{
+                firebaseAuth.getCurrentUser().delete().addOnSuccessListener(runnable -> {
+                    firebaseFirestore
+                            .collection("users")
+                            .document(UserCache.getUser(mContext).getEmail())
+                            .delete()
+                            .addOnSuccessListener(runnable1 -> {
+                                Toast.makeText(mContext, "정상적으로 탈퇴되었습니다!",Toast.LENGTH_SHORT).show();
+                                logout();
+                            });
+                });
+            });
+            builder.setNegativeButton("취소", (dialogInter, i) ->{});
+            builder.show();
+        });
 
         return binding.getRoot();
+    }
+    private void logout(){
+        UserCache.clear(mContext);
+
+        firebaseAuth.signOut();
+        startActivity(new Intent(mContext, LoginActivity.class));
+        getActivity().finish();
     }
 }
